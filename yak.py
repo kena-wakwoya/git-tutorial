ca7f7f109043ddf750bd73da394b48ecfa990a8b
kena wakwoya <kwakwoya@gmail.com>
2018-04-20 15:24:30 -0300

    konka

---
+++ yak.py
@@ -0,0 +1,12 @@
+list.append(x) = append an item at the end o;
+a[len(a):] = [x]
+another thing is list.extend(iterable);
+list.insert(i,x) = here x is the 
+ x represent the data we insert and the value of i  is where we have to insert the value
+ list.remove(x) == remove the first  value from the list if it exist
+
+ list.clear() remove all the value from the list
+ tessting is used for two main purposes
+    1. to demostrate right behavior
+    2 to deteectt defects or problems and fix them.
+test planning and preparation == includes to set the goal of the test,strategy of the test, testcases and test procedure
